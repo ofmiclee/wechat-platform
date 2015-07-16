3bd551d16d2ba455350c5b0eb9d6ecb28becd1e4
MicLee <liwenjun@qianmi.com>
2015-07-16 16:12:50 +0800

    modify message

--- test/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  appid: 'appid',
-  appsecret: 'appsecret'
-};
---
+++ test/message.test.js
@@ -0,0 +1,12 @@
+var config = {
+  appid: 'appid',
+
+  appsecret: 'appsecret',
+
+  msg_token: 'token',
+
+  msg_aeskey: 'aeskey'
+};
+
+var Message = require('../lib/message');
+// console.log(Message(config).proxy);
